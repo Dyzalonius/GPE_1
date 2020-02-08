6edf735086001802737ac6ce9bd3d267ccd67089
dyzalonius <dyzalonius@gmail.com>
2020-02-08 10:54:00 +0100

    Change UV placement to loop perfectly in X and Y, and added extra variables for vertices count to clarify for-loops

--- Assets/Scripts/Donut.cs
+++ Assets/Scripts/Donut.cs
@@ -7,12 +7,12 @@ public class Donut : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField]
-    [Range(2, 100)]
-    private int verticesPerCircle;
+    [Range(3, 100)]
+    private int donutSections;
 
     [SerializeField]
-    [Range(3, 100)]
-    private int sections;
+    [Range(2, 100)]
+    private int edgesPerSection;
 
     [SerializeField]
     private float radius;
@@ -21,7 +21,7 @@ public class Donut : MonoBehaviour
     private float thickness;
     
     [SerializeField]
-    private Vector2 textureSize; // make it so its a Vector2Int and it determines how many times the texture will wrap, regardless of vertexcount
+    private Vector2Int textureSize;
 
     [SerializeField]
     private Vector3 center;
@@ -30,6 +30,8 @@ public class Donut : MonoBehaviour
     private Vector3[] vertices;
     private Vector2[] uv;
     private int[] triangles;
+    private int verticesPerSectionCircle;
+    private int verticesPerDonutCircle;
 
     private void Start()
     {
@@ -39,13 +41,15 @@ public class Donut : MonoBehaviour
 
     private void Update()
     {
+        verticesPerSectionCircle = edgesPerSection + 1;
+        verticesPerDonutCircle = donutSections + 1;
         UpdateDonut();
     }
 
     private void UpdateDonut()
     {
         mesh.Clear();
-        
+
         FindVertices();
         FindUV();
         FindTriangles();
@@ -58,57 +62,56 @@ public class Donut : MonoBehaviour
 
     private void FindVertices()
     {
-        vertices = new Vector3[(sections + 1) * (verticesPerCircle + 1)];
+        vertices = new Vector3[verticesPerDonutCircle * (edgesPerSection + 1)];
 
-        for (int i = 0; i < (sections + 1); i++)
-            for (int j = 0; j <= verticesPerCircle; j++)
+        for (int i = 0; i < verticesPerDonutCircle; i++)
+            for (int j = 0; j < verticesPerSectionCircle; j++)
             {
                 Vector3 pos = new Vector3
                 {
-                    x = center.x + thickness * Mathf.Cos(2 * Mathf.PI / verticesPerCircle * j) * Mathf.Cos(2 * Mathf.PI / sections * i) + radius * Mathf.Cos(2 * Mathf.PI / sections * i),
-                    y = center.y + thickness * Mathf.Sin(2 * Mathf.PI / verticesPerCircle * j),
-                    z = center.z + thickness * Mathf.Cos(2 * Mathf.PI / verticesPerCircle * j) * Mathf.Sin(2 * Mathf.PI / sections * i) + radius * Mathf.Sin(2 * Mathf.PI / sections * i)
+                    x = center.x + thickness * Mathf.Cos(2 * Mathf.PI / edgesPerSection * j) * Mathf.Cos(2 * Mathf.PI / donutSections * i) + radius * Mathf.Cos(2 * Mathf.PI / donutSections * i),
+                    y = center.y + thickness * Mathf.Sin(2 * Mathf.PI / edgesPerSection * j),
+                    z = center.z + thickness * Mathf.Cos(2 * Mathf.PI / edgesPerSection * j) * Mathf.Sin(2 * Mathf.PI / donutSections * i) + radius * Mathf.Sin(2 * Mathf.PI / donutSections * i)
                 };
-                vertices[i * verticesPerCircle + j] = pos;
+                vertices[i * verticesPerSectionCircle + j] = pos;
             }
     }
 
     private void FindUV()
     {
-        uv = new Vector2[(sections + 1) * (verticesPerCircle + 1)];
+        uv = new Vector2[verticesPerDonutCircle * verticesPerSectionCircle];
 
-        for (int i = 0; i < (sections + 1); i++)
-            for (int j = 0; j <= verticesPerCircle; j++)
+        for (int i = 0; i < verticesPerDonutCircle; i++)
+            for (int j = 0; j < verticesPerSectionCircle; j++)
             {
-                Vector2 newUV = new Vector2(i / textureSize.x, j / textureSize.y); //Problem is that if value goes over 1, between vertex 9 and 0, it will go from e.g. uv 20 to 0 in a single face. Fix problem by adding extra set of vertices by making for loop <=, remove modulo from FindTriangles to instead of wrap make it to the last vertices.
-                uv[i * verticesPerCircle + j] = newUV;
+                Vector2 newUV = new Vector2(((float) textureSize.x / donutSections) * i, ((float) textureSize.y / edgesPerSection) * j); //Problem is that if value goes over 1, between vertex 9 and 0, it will go from e.g. uv 20 to 0 in a single face. Fix problem by adding extra set of vertices by making for loop <=, remove modulo from FindTriangles to instead of wrap make it to the last vertices.
+                uv[i * verticesPerSectionCircle + j] = newUV;
             }
     }
     
     private void FindTriangles()
     {
-        triangles = new int[sections * verticesPerCircle * 6];
-        
-        for (int i = 0; i < sections; i++)
-        {
-            for (int j = 0; j < verticesPerCircle; j++)
+        triangles = new int[donutSections * edgesPerSection * 6];
+
+        for (int i = 0; i < donutSections; i++)
+            for (int j = 0; j < edgesPerSection; j++)
             {
-                triangles[(((i * verticesPerCircle) + j) * 6)] = (i * verticesPerCircle + j);
-                triangles[(((i * verticesPerCircle) + j) * 6) + 1] = (i * verticesPerCircle + ((j + 1)));
-                triangles[(((i * verticesPerCircle) + j) * 6) + 2] = (i * verticesPerCircle + j + verticesPerCircle);
+                triangles[(((i * edgesPerSection) + j) * 6)] = (i * verticesPerSectionCircle + j);
+                triangles[(((i * edgesPerSection) + j) * 6) + 1] = (i * verticesPerSectionCircle + j + 1);
+                triangles[(((i * edgesPerSection) + j) * 6) + 2] = (i * verticesPerSectionCircle + j + verticesPerSectionCircle);
                 
-                triangles[(((i * verticesPerCircle) + j) * 6) + 3] = (i * verticesPerCircle + j + verticesPerCircle);
-                triangles[(((i * verticesPerCircle) + j) * 6) + 4] = (i * verticesPerCircle + ((j + 1)));
-                triangles[(((i * verticesPerCircle) + j) * 6) + 5] = (i * verticesPerCircle + ((j + 1)) + verticesPerCircle);
+                triangles[(((i * edgesPerSection) + j) * 6) + 3] = (i * verticesPerSectionCircle + j + verticesPerSectionCircle);
+                triangles[(((i * edgesPerSection) + j) * 6) + 4] = (i * verticesPerSectionCircle + j + 1);
+                triangles[(((i * edgesPerSection) + j) * 6) + 5] = (i * verticesPerSectionCircle + j + 1 + verticesPerSectionCircle);
             }
-        }
+
     }
 
     void OnDrawGizmosSelected()
     {
         for (int i = 0; i < vertices.Length; i++)
         {
-            Gizmos.color = new Color(0, (float)i / vertices.Length, 0);
+            Gizmos.color = new Color(0, (float)i % (float)(edgesPerSection + 1) / (float)(edgesPerSection + 1), 0);
             Gizmos.DrawSphere(vertices[i], 0.5f);
         }
     }
